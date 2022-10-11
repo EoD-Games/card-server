d6dd4ef5a607687b9c7ac164d13e98533d7b3b1b
IsLilyYaGirl <35901613+IsLilyYaGirl@users.noreply.github.com>
2022-10-11 20:35:00 +0100

    Switch to JSON for protocol

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ezoauth;
 use reqwest;
-use serde_json::Value;
+use serde_json::{json, Value};
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::env;
@@ -26,7 +26,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			};
 			let (rx, auth_url) = ezoauth::authenticate(config, "localhost:8696").expect("Failed to authenticate");
 			println!("Client should authenticate at {auth_url}");
-			socket.write_all().await.expect("Failed to send OAuth URL");
+			socket.write_all(json! ({
+				"type": "oauth",
+				"oauth_url": auth_url
+			}).to_string().as_bytes()).await.expect("Failed to send OAuth URL");
 			let ores = rx.recv().unwrap().expect("No token");
 			let token = ores.access_token();
 			let res: Value = serde_json::from_str(&reqwest::Client::new().get("https://discord.com/api/users/@me")
